80cef73164d0e5043bb241e8e48a6f1b2666fae6
Daniele Fonseca <106083414+danisfon@users.noreply.github.com>
2025-05-12 16:48:48 -0300

    commit do site feito para sortear os nomes

---
+++ script.js
@@ -0,0 +1,38 @@
+let nomes = [];
+
+function adicionarNome() {
+    const nome = document.getElementById('nome');
+    const nomeValido = nome.value.trim();
+
+    if (nomeValido) {
+        nomes.push(nomeValido);
+        atualizarLista();
+        nome.value = '';
+        nome.focus();
+    } else {
+        alert('Digite um nome válido!');
+    }
+}
+
+function atualizarLista() {
+    const listaNome = document.getElementById('listaNome');
+    listaNome.innerHTML = '';
+
+    nomes.forEach((nomeValido, index) => {
+        const li = document.createElement('li');
+        li.textContent = nomeValido;
+        listaNome.appendChild(li);
+    });
+}
+
+function sortearNome() {
+    if (nomes.length === 0) {
+        alert('Adicione ao menos um nome antes de sortear!');
+        return;
+    }
+
+    const nomeAleatorio = Math.floor(Math.random() * nomes.length);
+    const selecionandoNome = nomes[nomeAleatorio];
+
+    document.getElementById('resultado').textContent = `Sorteado: ${selecionandoNome}`;
+}
